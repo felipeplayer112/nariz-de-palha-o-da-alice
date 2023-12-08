7c03b725ffe0fbdb47dee8c5fc2c8cf3925757e9
felipeplayer112 <131708175+felipeplayer112@users.noreply.github.com>
2023-12-08 17:16:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+noseX = 0;
+noseY = 0;
+
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(400, 400);
+    canvas.center();
+}
+
+function draw() {
+
+}
+
+function take_selfie() {
+    save("Foto.png");
+}
